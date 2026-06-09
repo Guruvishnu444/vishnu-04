bb3ed9be3b2dae7ba754ed7664dde43caf1d05b7
Guruvishnu <guruvishnu4gd@gmail.com>
2026-06-09 19:36:39 +0530

    Add Journey, remove CursorGlow

--- src/components/CursorGlow.jsx
+++
@@ -1,9 +0,0 @@
-import { useCursorGlow } from '../hooks/useCursorGlow'
-
-function CursorGlow() {
-  useCursorGlow()
-  
-  return <div id="cursor-glow" aria-hidden="true" />
-}
-
-export default CursorGlow
